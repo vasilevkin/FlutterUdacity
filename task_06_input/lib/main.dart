2beba76f486f1ee8c187195939ef53b5160e3881
Sergey Vasilevkin <svasilev@mera.ru>
2019-05-07 07:36:12 +0300

    Resolve task_06_input.

--- task_06_input/lib/main.dart
+++ task_06_input/lib/main.dart
@@ -23,8 +23,16 @@ class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Unit Converter',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
+      ),
       // TODO: Fill out the theme parameter
       home: CategoryRoute(),
     );
