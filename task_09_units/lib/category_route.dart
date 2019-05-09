79ced3ae3fc875f3a0d5245bdd07e055971a89c7
Sergey Vasilevkin <svasilev@mera.ru>
2019-05-09 11:29:56 +0300

    Resolve task_09_units.

--- task_09_units/lib/category_route.dart
+++ task_09_units/lib/category_route.dart
@@ -37,16 +37,16 @@ class _CategoryRouteState extends State<CategoryRoute> {
   // For more details, see https://github.com/dart-lang/sdk/issues/27755
   final _categories = <Category>[];
   // TODO: Remove _categoryNames as they will be retrieved from the JSON asset
-  static const _categoryNames = <String>[
-    'Length',
-    'Area',
-    'Volume',
-    'Mass',
-    'Time',
-    'Digital Storage',
-    'Energy',
-    'Currency',
-  ];
+//  static const _categoryNames = <String>[
+//    'Length',
+//    'Area',
+//    'Volume',
+//    'Mass',
+//    'Time',
+//    'Digital Storage',
+//    'Energy',
+//    'Currency',
+//  ];
   static const _baseColors = <ColorSwatch>[
     ColorSwatch(0xFF6AB7A8, {
       'highlight': Color(0xFF6AB7A8),
@@ -85,34 +85,34 @@ class _CategoryRouteState extends State<CategoryRoute> {
 
   // TODO: Remove the overriding of initState(). Instead, we use
   // didChangeDependencies()
-  @override
-  void initState() {
-    super.initState();
-    for (var i = 0; i < _categoryNames.length; i++) {
-      var category = Category(
-        name: _categoryNames[i],
-        color: _baseColors[i],
-        iconLocation: Icons.cake,
-        units: _retrieveUnitList(_categoryNames[i]),
-      );
-      if (i == 0) {
-        _defaultCategory = category;
-      }
-      _categories.add(category);
-    }
-  }
+//  @override
+//  void initState() {
+//    super.initState();
+//    for (var i = 0; i < _categoryNames.length; i++) {
+//      var category = Category(
+//        name: _categoryNames[i],
+//        color: _baseColors[i],
+//        iconLocation: Icons.cake,
+//        units: _retrieveUnitList(_categoryNames[i]),
+//      );
+//      if (i == 0) {
+//        _defaultCategory = category;
+//      }
+//      _categories.add(category);
+//    }
+//  }
 
   // TODO: Uncomment this out. We use didChangeDependencies() so that we can
   // wait for our JSON asset to be loaded in (async).
-  //  @override
-  //  Future<void> didChangeDependencies() async {
-  //    super.didChangeDependencies();
-  //    // We have static unit conversions located in our
-  //    // assets/data/regular_units.json
-  //    if (_categories.isEmpty) {
-  //      await _retrieveLocalCategories();
-  //    }
-  //  }
+    @override
+    Future<void> didChangeDependencies() async {
+      super.didChangeDependencies();
+      // We have static unit conversions located in our
+      // assets/data/regular_units.json
+      if (_categories.isEmpty) {
+        await _retrieveLocalCategories();
+      }
+    }
 
   /// Retrieves a list of [Categories] and their [Unit]s
   Future<void> _retrieveLocalCategories() async {
@@ -125,6 +125,27 @@ class _CategoryRouteState extends State<CategoryRoute> {
     if (data is! Map) {
       throw ('Data retrieved from API is not a Map');
     }
+
+    var categoryIndex = 0;
+    data.keys.forEach((key) {
+      final List<Unit> units = data[key].map<Unit>((dynamic data) => Unit.fromJson(data)).toList();
+
+      var category = Category(
+        name: key,
+        units: units,
+        color: _baseColors[categoryIndex],
+        iconLocation: Icons.cake,
+      );
+      setState(() {
+        if (categoryIndex == 0) {
+          _defaultCategory = category;
+        }
+        _categories.add(category);
+      });
+      categoryIndex += 1;
+    });
+
+
     // TODO: Create Categories and their list of Units, from the JSON asset
   }
 
@@ -167,16 +188,16 @@ class _CategoryRouteState extends State<CategoryRoute> {
   // TODO: Delete this function; instead, read in the units from the JSON asset
   // inside _retrieveLocalCategories()
   /// Returns a list of mock [Unit]s.
-  List<Unit> _retrieveUnitList(String categoryName) {
-    // when the app first starts up
-    return List.generate(10, (int i) {
-      i += 1;
-      return Unit(
-        name: '$categoryName Unit $i',
-        conversion: i.toDouble(),
-      );
-    });
-  }
+//  List<Unit> _retrieveUnitList(String categoryName) {
+//    // when the app first starts up
+//    return List.generate(10, (int i) {
+//      i += 1;
+//      return Unit(
+//        name: '$categoryName Unit $i',
+//        conversion: i.toDouble(),
+//      );
+//    });
+//  }
 
   @override
   Widget build(BuildContext context) {
